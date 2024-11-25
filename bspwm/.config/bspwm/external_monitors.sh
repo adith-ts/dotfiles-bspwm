56df9da5c894ff7035be247f10dac6b6da8fe088
Adith T S <adithts@gmail.com>
2024-11-25 14:56:54 +0530

    add bspwm nitrogen nvim picom ranger rofi sxhkd configs

---
+++ bspwm/.config/bspwm/external_monitors.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+bspc monitor eDP -d I II III IV
+# Second monitor
+if [[ $(xrandr -q | grep 'HDMI-1-0 connected') ]]; then
+    xrandr --output eDP --primary --mode 1920x1080 --rotate normal --output HDMI1 --mode 1600x900 --rotate normal --left-of eDP
+    # Workspaces
+    bspc monitor HDMI-1-0 -d V VI VII VIII
+else
+    xrandr --output HDMI1 --off
+    # Remove nodes
+    while bspc node @V: --kill; do
+        :
+    done
+    while bspc node @VI: --kill; do
+        :
+    done
+    while bspc node @VII: --kill; do
+        :
+    done
+    while bspc node @VIII: --kill; do
+        :
+    done
+    # Remove workspaces
+    bspc desktop -r V VI VII VIII
+    # Remove monitor
+    bspc monitor HDMI1 -r
+fi
